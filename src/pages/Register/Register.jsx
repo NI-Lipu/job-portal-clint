17f4d5fa60c2e6a059ba0d18c968e437e5d2e6b2
NI Lipu <nilipu500@gmail.com>
2025-06-21 21:39:53 +0600

    Add Password validation and show sweet alert

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,25 @@
 import Lottie from 'lottie-react'
 import registerLottieAnimation from '../../assets/lottie/register.json'
+import Swal from 'sweetalert2'
 
 const Register = () => {
+   const handleRegister = (e) => {
+      e.preventDefault()
+      const form = e.target
+      const email = form.email.value
+      const password = form.password.value
+
+      // Password Validation
+      const regex = /^(?=.*[A-Z])(?=.*\d).{6,}$/
+      if (!regex.test(password)) {
+         Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: '"Password must be at least 6 characters long and include at least one uppercase letter and one number."!',
+         })
+      }
+      console.log(email, password)
+   }
    return (
       <div className="hero bg-base-200 min-h-screen">
          <div className="hero-content flex-col lg:flex-row-reverse">
@@ -10,13 +28,14 @@ const Register = () => {
             </div>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                <h1 className="text-5xl font-bold ml-5 mt-4">Register now!</h1>
-               <form className="card-body">
+               <form onSubmit={handleRegister} className="card-body">
                   <div className="form-control">
                      <label className="label">
                         <span className="label-text">Email</span>
                      </label>
                      <input
                         type="email"
+                        name="email"
                         placeholder="email"
                         className="input input-bordered"
                         required
@@ -28,15 +47,11 @@ const Register = () => {
                      </label>
                      <input
                         type="password"
+                        name="password"
                         placeholder="password"
                         className="input input-bordered"
                         required
                      />
-                     <label className="label">
-                        <a href="#" className="label-text-alt link link-hover">
-                           Forgot password?
-                        </a>
-                     </label>
                   </div>
                   <div className="form-control mt-6">
                      <button className="btn btn-primary">Register</button>
