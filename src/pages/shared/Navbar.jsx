f4e388f5705edd354ccf4513997bd76c8ec6214f
NI Lipu <nilipu500@gmail.com>
2025-06-21 18:04:54 +0600

    Add daisyui Register form in the register page and place the register.json animation in the assets

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -43,7 +43,9 @@ const Navbar = () => {
                   {links}
                </ul>
             </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+            <Link to="/" className="btn btn-ghost text-xl">
+               daisyUI
+            </Link>
          </div>
          <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">{links}</ul>
