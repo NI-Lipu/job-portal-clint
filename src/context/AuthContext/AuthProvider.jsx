42f7294bec9c80279432feb353939c4cb05fe7e1
NI Lipu <nilipu500@gmail.com>
2025-06-22 07:13:17 +0600

    Add user, loading state and Observer in the AuthProvider component

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AuthContext from './AuthContext'
+import { onAuthStateChanged } from 'firebase/auth'
+import auth from '../../firebase/firebase.config/'
 
 const AuthProvider = ({ children }) => {
    const [user, setUser] = useState(null)
-   const [loader, setLoader] = useState(true)
+   const [loading, setLoading] = useState(true)
 
-   const authInfo = { user, loader }
+   // Set Observer
+   useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+         setUser(currentUser)
+         setLoading(false)
+      })
+      return () => {
+         unsubscribe()
+      }
+   }, [])
+
+   const authInfo = { user, loading }
    return (
       <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
    )
